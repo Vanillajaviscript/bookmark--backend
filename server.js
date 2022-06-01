699a78f994233a7899c2d79dab1c39afd4e495e4
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-01 09:28:52 -0700

    adds put and delete routes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
+const colors = require('colors');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const logger = require('morgan');
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
-const Bookmark = require('./models/Bookmark')
+const Bookmark = require('./models/Bookmark');
+
 
 app.use(express.json());
 app.use(cors());
@@ -15,20 +17,40 @@ app.get('/', (req, res) => {
   res.status(200).json('root directory')
 });
 
-app.get('/bookmarks', async(req, res) => {
+//Get all bookmarks
+app.get('/bookmarks', async (req, res) => {
   try {
     res.json(await(await Bookmark.find({})).reverse())
   } catch (error) {
     res.status(400).json(error)
   }
-})
+});
 
+//Delete bookmark
+app.delete('/bookmarks/:id', async (req, res) => {
+  try {
+    res.json(await Bookmark.findByIdAndDelete(req.params.id))
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+//Update bookmark
+app.put('/bookmarks/:id', async (req, res) => {
+  try {
+    res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+//Create bookmark
 app.post('/bookmarks', async(req, res) => {
   try{  res.status(201).json(await Bookmark.create(req.body))
   } catch (error) {
     res.status(400).json(error)
   }
-})
+});
 
 
 app.listen(PORT, () => {
