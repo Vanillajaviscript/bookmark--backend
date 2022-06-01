956d8c1ecdd8f7a05228ce096aba87ddf03eaa31
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-31 22:23:24 -0700

    adds root route and dependencies

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.status(200).json('root directory')
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is live on Port: ${PORT}`)
+});
