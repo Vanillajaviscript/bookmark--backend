3a82e6772b39197fd86f5ee7bec3b7b3684859df
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-31 23:03:45 -0700

    adds get and post bookmark route

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ app.get('/', (req, res) => {
   res.status(200).json('root directory')
 });
 
+app.get('/bookmarks', async(req, res) => {
+  try {
+    res.json(await(await Bookmark.find({})).reverse())
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+app.post('/bookmarks', async(req, res) => {
+  try{  res.status(201).json(await Bookmark.create(req.body))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server is live on Port: ${PORT}`)
 });
