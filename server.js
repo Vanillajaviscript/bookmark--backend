9322067fc8e1033da35663f1726ce1b6199aebaa
contactharrisc2 <contactharrisc2@gmail.com>
2022-06-01 00:52:04 -0500

    adds pseudocode and server tested

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/////////////////
+// DEPENDENCIES
+////////////////
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -6,14 +9,25 @@ const logger = require('morgan');
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 
+///////////////////
+// MIDDLEWARE
+///////////////////
 app.use(express.json());
 app.use(cors());
 app.use(logger('dev'));
 
+
+//////////////
+// ROUTES
+/////////////
 app.get('/', (req, res) => {
   res.status(200).json('root directory')
 });
 
+
+//////////////////
+// SERVER LISTENER
+//////////////////
 app.listen(PORT, () => {
   console.log(`Server is live on Port: ${PORT}`)
 });
