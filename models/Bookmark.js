8f95b2c10958b4dc0ea6de28689771ebaf727694
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-06-01 17:31:49 -0700

    adds timestamps to schema

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const linkSchema = new mongoose.Schema({
     title : {type: String},
     url: {type: String}
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model('Link', linkSchema);
